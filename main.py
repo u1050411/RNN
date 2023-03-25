60605373344d924154ae0de7db7da9a7877495ec
u1050411@gmail.com <@Pjnuka2001>
2023-03-25 12:36:01 +0100

    Añadido que las capas , las neuronas y las iteraciones se busquen las mas optimas

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import LSTM
 import matplotlib.pyplot as plt
 import os
+import optuna
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.metrics import mean_squared_error
 
 
 class RNNModel:
@@ -72,41 +75,62 @@ class RNNModel:
         self.X_train_transformed = self.preprocessor.fit_transform(self.X_train)
         self.X_test_transformed = self.preprocessor.transform(self.X_test)
 
+    def create_model(self, trial=None, params=None):
+        if trial is not None:
+            n_layers = trial.suggest_int("n_layers", 1, 3)
+        else:
+            n_layers = params["n_layers"]
+
+        model = Sequential()
+        for i in range(n_layers):
+            if trial is not None:
+                num_units = trial.suggest_int(f"num_units_layer_{i + 1}", 10, 100)
+            else:
+                num_units = params[f"num_units_layer_{i + 1}"]
+
+            if i == 0:
+                model.add(LSTM(num_units, activation='relu', input_shape=(self.X_train_transformed.shape[1], 1),
+                               return_sequences=True if n_layers > 1 else False))
+            elif i < n_layers - 1:
+                model.add(LSTM(num_units, activation='relu', return_sequences=True))
+            else:
+                model.add(LSTM(num_units, activation='relu'))
+
+        model.add(Dense(len(self.num_features)))
+
+        if trial is not None:
+            lr = trial.suggest_float("lr", 1e-5, 1e-2, log=True)
+        else:
+            lr = params["lr"]
+
+        model.compile(optimizer=Adam(learning_rate=lr), loss='mse')
+        return model
+
+    def objective(self, trial):
+        model = self.create_model(trial)
+        early_stop = EarlyStopping(monitor='val_loss', patience=5)
+        history = model.fit(
+            self.X_train_transformed.reshape(-1, self.X_train_transformed.shape[1], 1),
+            self.y_train,
+            epochs=100,
+            verbose=1,
+            validation_data=(self.X_test_transformed.reshape(-1, self.X_test_transformed.shape[1], 1), self.y_test),
+            callbacks=[early_stop],
+        )
+        return history.history["val_loss"][-1]
+
     def train_model(self):
-        """Entrena el model"""
-        self.model = Sequential()
-        self.model.add(
-            LSTM(64, input_shape=(self.X_train_transformed.shape[1], 1), activation='relu', return_sequences=True))
-        self.model.add(LSTM(64, activation='relu', return_sequences=True))
-        self.model.add(LSTM(32, activation='relu'))
-        self.model.add(Dense(64, activation='relu'))
-        self.model.add(Dense(len(self.num_features), activation='linear'))
-        self.model.compile(optimizer=Adam(learning_rate=0.001), loss='mse')
+        study = optuna.create_study(direction="minimize")
+        study.optimize(self.objective, n_trials=50)
 
-        # Crear un directorio para guardar los gráficos
-        if not os.path.exists('graficos'):
-            os.makedirs('graficos')
-
-        for epoch in range(200):
-            print(f"Epoch: {epoch + 1}")
-            self.model.fit(self.X_train_transformed.reshape(-1, self.X_train_transformed.shape[1], 1), self.y_train,
-                           epochs=1, verbose=1)
-
-            # Realizar predicciones en el conjunto de prueba
-            predictions = self.model.predict(self.X_test_transformed.reshape(-1, self.X_test_transformed.shape[1], 1))
-
-            # Crear gráfico comparando los datos reales
-            fig, ax = plt.subplots(len(self.num_features), 1, figsize=(12, len(self.num_features) * 4))
-
-            for idx, feature in enumerate(self.num_features):
-                ax[idx].plot(self.y_test.index, self.y_test[feature], label='Real')
-                ax[idx].plot(self.y_test.index, predictions[:, idx], label='Predicción', linestyle='--')
-                ax[idx].set_title(f"{feature} - Epoch {epoch + 1}")
-                ax[idx].legend()
-
-            # Guardar el gráfico en un archivo
-            fig.savefig(f"graficos/grafico_epoch_{epoch + 1}.png")
-            plt.close(fig)
+        self.best_params = study.best_params
+        self.model = self.create_model(params=self.best_params)
+        self.model.fit(
+            self.X_train_transformed.reshape(-1, self.X_train_transformed.shape[1], 1),
+            self.y_train,
+            epochs=100,
+            verbose=1
+        )
 
     def adjust_data(self):
         """Ajusta les dades"""
@@ -164,12 +188,22 @@ class RNNModel:
         # Guarda les prediccions en un arxiu Excel
         self.save_predictions()
 
-
     def save_predictions(self):
         """Guarda les prediccions en un arxiu Excel"""
         output_file = "prediccions_RNNModel_2023.xlsx"
-        self.future_data[self.num_features] = self.future_predictions
+        self.future_data[self.num_features[0]] = self.future_predictions
         self.future_data.to_excel(output_file, engine='openpyxl', index=False)
+
+    def plot_comparison(self):
+        y_pred = self.model.predict(self.X_test_transformed.reshape(-1, self.X_test_transformed.shape[1], 1))
+        plt.figure()
+        plt.plot(self.y_test.reset_index(drop=True), label="Real")
+        plt.plot(y_pred, label="Predicho")
+        plt.legend()
+        plt.xlabel("Tiempo")
+        plt.ylabel("Valor")
+        plt.title("Comparación de datos reales y predichos")
+        plt.savefig("grafico_final.png")
 
 
 if __name__ == '__main__':
@@ -183,3 +217,4 @@ if __name__ == '__main__':
     predictor.train_model()
     predictor.generate_future_data()
     predictor.predict_future_data()
+    predictor.plot_comparison()
